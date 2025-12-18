10f16a0290a12e66fb68972b20fb75a4d033587f
ffdly <fadly.nirwan17@gmail.com>
2025-12-18 13:39:01 +0700

    Update convertersuhubeta.go

--- convertersuhubeta.go
+++ convertersuhubeta.go
@@ -1,39 +1,41 @@
 package main
 
-
 import "fmt"
 
 func main() {
 	var pilihan int
 	var suhu float64
 
-	fmt.Println("=== Converter Suhu ===")
-	fmt.Println("1. Celcius ke Fahrenheit")
-	fmt.Println("2. Celcius ke Kelvin")
-	fmt.Println("3. Fahrenheit ke Celcius")
-	fmt.Println("4. Kelvin ke Celcius")
-	fmt.Print("Pilih menu (1-4): ")
-	fmt.Scan(&pilihan)
+	for {
+		fmt.Println("\n=== Converter Suhu ===")
+		fmt.Println("1. Celcius ke Fahrenheit")
+		fmt.Println("2. Celcius ke Kelvin")
+		fmt.Println("3. Fahrenheit ke Celcius")
+		fmt.Println("4. Kelvin ke Celcius")
+		fmt.Println("0. Keluar")
+		fmt.Print("Pilih menu: ")
+		fmt.Scan(&pilihan)
 
-	if pilihan == 0 {
-		fmt.println("program kalkulasi selesai.")
-		break
-	}
+		if pilihan == 0 {
+			fmt.Println("Kalkulasi Selesai.")
+			break
+		}
 
-	fmt.Print("Masukkan nilai suhu: ")
-	fmt.Scan(&suhu)
+		fmt.Print("Masukkan nilai suhu: ")
+		fmt.Scan(&suhu)
 
-	switch pilihan {
-	case 1:
-		fmt.Printf("Hasil: %.2f °F\n", celciusToFahrenheit(suhu))
-	case 2:
-		fmt.Printf("Hasil: %.2f K\n", celciusToKelvin(suhu))
-	case 3:
-		fmt.Printf("Hasil: %.2f °C\n", fahrenheitToCelcius(suhu))
-	case 4:
-		fmt.Printf("Hasil: %.2f °C\n", kelvinToCelcius(suhu))
-	default:
-		fmt.Println("Pilihan tidak valid")
+		switch pilihan {
+		case 1:
+			fmt.Printf("Hasil: %.2f °F\n", celciusToFahrenheit(suhu))
+		case 2:
+			fmt.Printf("Hasil: %.2f K\n", celciusToKelvin(suhu))
+		case 3:
+			fmt.Printf("Hasil: %.2f °C\n", fahrenheitToCelcius(suhu))
+		case 4:
+			fmt.Printf("Hasil: %.2f °C\n", kelvinToCelcius(suhu))
+		default:
+			fmt.Println("Pilihan tidak valid")
+		}
 	}
 }
 
@@ -53,4 +55,3 @@ func fahrenheitToCelcius(f float64) float64 {
 func kelvinToCelcius(k float64) float64 {
 	return k - 273.15
 }
-
